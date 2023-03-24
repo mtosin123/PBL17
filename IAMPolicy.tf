64f8c726eaa413fe1f6543b0bd37244a6474663c
mtosin23 <mtosin23@yahoo.com>
2023-03-24 19:50:45 +0100

    TFproject

---
+++ IAMPolicy.tf
@@ -0,0 +1,27 @@
+#######################################
+#Creating IAM Policy
+resource "aws_iam_policy" "policy" {
+  name        = "ec2_instance_policy"
+  description = "A test policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+
+  })
+
+  tags = merge(
+    var.tags,
+    {
+      Name =  "aws assume policy"
+    },
+  )
+
+}
